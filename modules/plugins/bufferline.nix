58659c80355f5f74ad64949563887e7802eba4c7
r-happy <106812882+r-happy@users.noreply.github.com>
2026-07-11 17:31:40 +0900

    update keymap

--- modules/plugins/bufferline.nix
+++ modules/plugins/bufferline.nix
@@ -2,4 +2,19 @@
   plugins.bufferline = {
     enable = true;
   };
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<C-b>n";
+      action = "<cmd>BufferLineCycleNext<cr>";
+      options.desc = "Next buffer";
+    }
+    {
+      mode = "n";
+      key = "<C-b>p";
+      action = "<cmd>BufferLineCyclePrev<cr>";
+      options.desc = "Previous buffer";
+    }
+  ];
 }
